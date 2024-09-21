efa506cc834474998b1654f406d2f3bc38f7fe9c
LuisLoboR16 <Lwolf95@hotmail.com>
2024-09-21 11:52:49 -0500

    added search 9 #13

---
+++ src/test/java/es/upm/miw/iwvg_devops/code/SearchesTest.java
@@ -0,0 +1,18 @@
+package es.upm.miw.iwvg_devops.code;
+
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SearchesTest {
+
+    @Test
+    void testFindHighestFraction() {
+        Fraction fraction = new Searches().findHighestFraction();
+        assertNotNull(fraction);
+        assertEquals(2, fraction.getNumerator());
+        assertEquals(1, fraction.getDenominator());
+    }
+}
